f74c3fded369ce26a5b5edcbb326969469e74ed1
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2020-07-24 18:40:25 -0400

    Handlers and events

---
+++ events/guild/message.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+let prefix = process.env.PREFIX; 
+
+module.exports = async (client, message) => {
+    
+  let args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  let cmd = args.shift().toLowerCase();
+
+  if (!message.content.startsWith(prefix)) return;
+  let commandfile =
+    client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+  if (commandfile) commandfile.run(client, message, args);
+};
